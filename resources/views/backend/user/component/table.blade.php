3512378050b2f9eb8c7a76e7ce324859e42bc355
Wo2mi <hoanganh.radiance@gmail.com>
2024-05-04 23:17:08 +0700

    css

---
+++ resources/views/backend/user/component/table.blade.php
@@ -0,0 +1,47 @@
+<table class="table table-striped table-bordered">
+    <thead>
+    <tr >
+        <th >
+            <input type="checkbox" value="" id="checkAll" class="input-checkbox">
+        </th>
+        <th >Họ Tên</th>
+        <th>Email</th>
+        <th>Số Điện Thoại</th>
+        <th >Địa Chỉ</th>
+        <th class="text-center">Tình Trạng</th>
+        <th class="text-center">Thao Tác</th>
+
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>
+            <input type="checkbox" value="" id="checkAll" class="input-checkbox checkBoxItem text-center">
+        </td>
+
+
+        <td>
+           Nguyễn Văn Khoa
+        </td>
+        <td>
+            kamenrider@gmail.com
+        </td>
+        <td>
+            0987654321
+        </td>
+        <td>
+            Số 16@#4@ Dương Nội, Hà Đông, Hà Nội
+        </td>
+        <td class="text-center">
+            <input type="checkbox" class="js-switch" checked />
+        </td>
+        <td class="text-center">
+                <a href="" class="btn btn-success"><i class="fa fa-edit"></i></a>
+                <a href="" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+        </td>
+        
+    </tr>
+  
+    </tr>
+    </tbody>
+</table>
